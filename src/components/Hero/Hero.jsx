b63fccb0d212dd8507e19d4fa5cd0d75dd1be7fd
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-02-18 20:47:39 +0200

    Created Hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>
+        моментально Копируй сделки профи трейдеров
+      </h1>
+    </div>
+  );
+}
