b92cdd04a48c133fba99bdb9cc67f0f42c915b85
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-02-19 14:30:54 +0200

    Created Numbers component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Numbers from "./components/Numbers/Numbers";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Numbers />
     </>
   );
 }
---
+++ src/components/Numbers/Numbers.jsx
@@ -0,0 +1,15 @@
+import styles from "./Numbers.module.css";
+
+export default function Numbers() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.rectangle}></div>
+      <div className={styles.container}>
+        <div>
+          <h2 className={styles.title}>Цифры</h2>
+          <p className={styles.date_text}>Cентябрь 2022</p>
+        </div>
+      </div>
+    </div>
+  );
+}
