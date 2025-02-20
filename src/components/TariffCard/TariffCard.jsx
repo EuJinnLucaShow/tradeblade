679213949c295bffd7477fd0460c52fb12b3dd4d
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-02-20 22:32:43 +0200

    Updated Tariffs component

--- src/components/TariffCard/TariffCard.jsx
+++ src/components/TariffCard/TariffCard.jsx
@@ -1,32 +1,15 @@
+import PropTypes from "prop-types";
+
 import Checkbox from "../../icons/Checkbox";
 import DottedLine from "../../icons/DottedLine";
 import DropDownList from "../../modules/DropDownList/DropDownList";
 
 import styles from "./TariffCard.module.css";
 
-const items = [
-  {
-    id: 1,
-    value: "Ручной трейдинг",
-  },
-  {
-    id: 2,
-    value: "Автоматическое или полуавтоматическое копирование сделок",
-  },
-  {
-    id: 3,
-    value: "Личный кабинет со статистикой",
-  },
-  {
-    id: 4,
-    value: "Среднесрочные сделки с уровнями набора портфеля",
-  },
-];
-
-export default function TariffCard() {
+export default function TariffCard({items, title, price, bgColor }) {
   return (
-    <div className={styles.wrapper}>
-      <h3 className={styles.title}>STANDART</h3>
+    <div className={styles.wrapper} style={{ background: `${bgColor}` }}>
+      <h3 className={styles.title}>{title}</h3>
       <ul className={styles.list}>
         {items.map((item) => (
           <div key={item.id} className={styles.wrap_item}>
@@ -40,12 +23,24 @@ export default function TariffCard() {
       </ul>
       <div className={styles.wrap_cost}>
         <div className={styles.cost}>
-          $234<div className={styles.discount}>-35%</div>
+          {price}
+          <div className={styles.discount}>-35%</div>
         </div>
         <div className={styles.term}>
           <DropDownList />
         </div>
       </div>
+      <button className={styles.button}>
+        <span>Попробовать</span>
+        <span className={styles.button_span}>5 дней бесплатно</span>
+      </button>
     </div>
   );
 }
+
+TariffCard.propTypes = {
+  items: PropTypes.array,
+  title: PropTypes.string,
+  price: PropTypes.string,
+  bgColor: PropTypes.string,
+};
--- src/components/Tariffs/Tariffs.jsx
+++ src/components/Tariffs/Tariffs.jsx
@@ -1,4 +1,7 @@
 import TariffCard from "../TariffCard/TariffCard";
+import { standartList } from "../../data/tradingStandart";
+import { vipList } from "../../data/tradingVip";
+
 import styles from "./Tariffs.module.css";
 
 export default function Tariffs() {
@@ -10,8 +13,18 @@ export default function Tariffs() {
         <button className={styles.futures}>фьючерс</button>
       </div>
       <div className={styles.wrap_card}>
-        <TariffCard />
-        <TariffCard />
+        <TariffCard
+          title={"STANDART"}
+          bgColor={""}
+          price={"$234"}
+          items={standartList}
+        />
+        <TariffCard
+          title={"VIP"}
+          bgColor={"#6A54FF"}
+          price={"$585"}
+          items={vipList}
+        />
       </div>
     </div>
   );
