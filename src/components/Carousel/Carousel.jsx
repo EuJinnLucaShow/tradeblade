af67a692091297e1ba9200f7a20ed1bf11a94c2a
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-02-21 16:03:34 +0200

    Install @react-hook/media-query

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,16 +1,20 @@
 import PropTypes from "prop-types";
 import Slider from "react-slick";
+import { useMediaQuery } from "@react-hook/media-query";
+
 import DealsCard from "../DealsCard/DealsCard";
 
 export default function Carousel({ deals }) {
+  const isMinWidth1024 = useMediaQuery("(min-width: 1024px)");
+
   const settings = {
     className: "center",
     arrows: false,
     dots: false,
     centerMode: true,
     infinite: true,
-    centerPadding: "32px",
-    slidesToShow: 1,
+    centerPadding: isMinWidth1024 ? "90px" : "32px",
+    slidesToShow: isMinWidth1024 ? 4 : 1,
     speed: 500,
     autoplay: true,
     variableWidth: false,
