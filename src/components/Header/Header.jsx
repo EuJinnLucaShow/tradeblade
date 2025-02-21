5ae3ed3adad2b654129ecdc6b77df350414f671d
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-02-21 17:22:33 +0200

    Update Header and Hero component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,25 @@
+import { useMediaQuery } from "@react-hook/media-query";
+
 import Logo from "../../icons/Logo";
 import HamburgerMenu from "../../modules/HamburgerMenu/HamburgerMenu";
 
 import styles from "./Header.module.css";
+import FooterNavList from "../../modules/FooterNavList/FooterNavList";
 
 export default function Header() {
+  const isMinWidth768 = useMediaQuery("(max-width: 768px)");
+
   return (
     <header className={styles.header}>
       <Logo width={"107.111"} height={"52"} />
-      <HamburgerMenu />
+      {!isMinWidth768 && <FooterNavList />}
+      {!isMinWidth768 && (
+        <div className={styles.btn_group}>
+          <button className={styles.log_btn}>вход</button>
+          <button className={styles.reg_btn}>регистрация</button>
+        </div>
+      )}
+      {isMinWidth768 && <HamburgerMenu />}
     </header>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,15 +6,17 @@ import styles from "./Hero.module.css";
 export default function Hero() {
   return (
     <div className={styles.wrapper}>
-      <img src={banner} alt="banner" />
-      <h1 className={styles.title}>
-        моментально Копируй сделки профи трейдеров
-      </h1>
-      <p className={styles.text}>
-        Начни копировать сделки с успешной командой профессиональных трейдеров в
-        автоматическом режиме.
-      </p>
-      <SignUpInput />
+      <img src={banner} alt="banner" className={styles.banner} />
+      <div className={styles.container}>
+        <h1 className={styles.title}>
+          моментально Копируй сделки профи трейдеров
+        </h1>
+        <p className={styles.text}>
+          Начни копировать сделки с успешной командой профессиональных трейдеров
+          в автоматическом режиме.
+        </p>
+        <SignUpInput />
+      </div>
     </div>
   );
 }
