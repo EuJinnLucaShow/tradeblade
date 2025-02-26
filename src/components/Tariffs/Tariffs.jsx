f41d85a4f8f786a42ba8a68526d0081a9fd944d3
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-02-26 10:17:44 +0200

    Updated component Tariffs

--- src/components/Tariffs/Tariffs.jsx
+++ src/components/Tariffs/Tariffs.jsx
@@ -3,15 +3,34 @@ import { standartList } from "../../data/tradingStandart";
 import { vipList } from "../../data/tradingVip";
 
 import styles from "./Tariffs.module.css";
+import { useState } from "react";
 
 export default function Tariffs() {
+  const [spot, setSpot] = useState(true);
+  const [futures, setFutures] = useState(false);
+
+  const handleClick = () => {
+    setSpot(!spot);
+    setFutures(!futures);
+  };
+
   return (
     <div className={styles.wrapper} id="tariffs">
       <div className={styles.container}>
         <h2 className={styles.title}>Tariffs</h2>
         <div className={styles.filter}>
-          <button className={styles.spot}>SPOT</button>
-          <button className={styles.futures}>Futures</button>
+          <button
+            className={spot ? styles.active : styles.inactive}
+            onClick={handleClick}
+          >
+            SPOT
+          </button>
+          <button
+            className={futures ? styles.active : styles.inactive}
+            onClick={handleClick}
+          >
+            Futures
+          </button>
         </div>
       </div>
       <div className={styles.wrap_card}>
